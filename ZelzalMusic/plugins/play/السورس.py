fdfbfb0aaed4973275e7e03171894206530b235b
⏤͟͞ َِ⍣⃟َ𝐓𝐇ِ𝐄 𓆩ََِِ𝐘َِْ𝐎ِِ𝐔ًٌُ𝐒𝐄ُِ𝐅𓆪 <167021222+yousef1yt@users.noreply.github.com>
2024-06-03 07:27:06 +0300

    Create السورس.py

---
+++ ZelzalMusic/plugins/play/السورس.py
@@ -0,0 +1,94 @@
+#𝙲𝙷.𝚂𝙾𝚄𝚁𝙲𝙴 : @SOURCE_EROR
+#𝙳𝙴𝚅 𝙼𝙰𝚉𝙴𝙽 : @Y_D_ll
+#𝚂𝚄𝙿𝙿𝙾𝚁𝚃 : @SOPER_EROR
+#MOHAMED تم التعديل بواسطة 🎸 ⋅
+from pyrogram.types import CallbackQuery
+import asyncio
+from asyncio import gather
+import os
+import time
+import requests
+from pyrogram import enums
+from pyrogram import types
+import aiohttp
+from pyrogram.types import CallbackQuery
+from pyrogram import filters
+from pyrogram import Client
+from pyrogram.enums import ChatMemberStatus
+from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton, ReplyKeyboardMarkup
+from MatrixMusic import (Apple, Resso, SoundCloud, Spotify, Telegram, YouTube, app)
+from MatrixMusic import app
+from telegraph import upload_file
+from asyncio import gather
+from pyrogram.errors import FloodWait
+
+
+
+##############################################################
+##############################################################
+          
+     
+@app.on_message(filters.command(["سورس","السورس","سورس مين","بلاك"], ""), group=221213)
+async def huhh(client: Client, message: Message):
+    await message.reply_video(
+        video=f"https://graph.org/file/fb68ae3b5a2e1d62fd94f.mp4",
+        caption=f"""• [⌯𝐃𝐄𝐕.𝐒𝐎𝐔𝐑𝐂𝐄⌯](https://t.me/y_o_v) •\n
+ [⌯𝐒𝐎𝐔𝐑𝐂𝐄 𝗕𝗹𝗮𝗰𝗸⌯](https://t.me/KKC8C)\n
+ [⌯𝐒𝐔𝐏𝐏𝐔𝐑𝐓.𝐒𝐎𝐔𝐑𝐂𝐄⌯](https://t.me/Senzir_Suuport)\n""",
+        reply_markup=InlineKeyboardMarkup(
+        [
+            [
+                    InlineKeyboardButton(
+                        "‹ 𝐃𝐄𝐕 𝐒𝐎𝐔𝐑𝐂𝐄 › ", url=f"https://t.me/y_o_v"),
+                ],[
+                    InlineKeyboardButton(
+                        "‹ 𝐒𝐎𝐔𝐑𝐂𝐄 ›", url=f"https://t.me/KKC8C"), 
+                    InlineKeyboardButton(
+                        "‹ 𝐒𝐔𝐏𝐏𝐔𝐑𝐓 ›", url=f"https://t.me/Senzir_Suuport"),
+                ],[
+                    InlineKeyboardButton(
+                        "‹ اضف بوت السورس الي مجموعتك ⚡️ ⋅ ›", url=f"http://t.me/F6Z_bot?startgroup=new"),
+            ]
+        ]
+         ),parse_mode=enums.ParseMode.MARKDOWN)
+
+
+
+@app.on_message(filters.command(["مطور السورس"], ""), group=221212)
+async def huhh(client: Client, message: Message):
+    await message.reply_photo(
+        photo=f"https://graph.org/file/8583d191f523ba70380da.jpg",
+        caption=f"""• ⌯ Developer Name : ˛ 𓆩ََِِ𝐘َِْ𝐎ِِ𝐔ًٌُ𝐒𝐄ُِ𝐅𓆪 ⌯ •\n- Devloper Username : @y_o_v Devloper id : 6094238403 ⋅""",
+        reply_markup=InlineKeyboardMarkup(
+        [
+            [
+                    InlineKeyboardButton(
+                        " ‹ قناة السورس ⋅ › ", url=f"https://t.me/KKC8C"),
+                ],[
+                    InlineKeyboardButton(
+                        "⏤͟͞ َِ⍣⃟َ𝐓𝐇ِ𝐄 𓆩ََِِ𝐘َِْ𝐎ِِ𝐔ًٌُ𝐒𝐄ُِ𝐅𓆪", url=f"https://t.me/y_o_v"), 
+                ],[
+                    InlineKeyboardButton(
+                        "‹ اضف بوت السورس الي مجموعتك ⚡️🎸 ⋅ ›", url=f"http://t.me/@6Z_bot?startgroup=new"),
+            ]
+        ]
+         ),
+     )
+
+               
+@app.on_message(filters.command(["اسمي","اسمي اي","قول اسمي"], ""), group=123222)
+async def vgdg(client: Client, message: Message):
+    await message.reply_text(
+        f"""- اسمك » ⦗ {message.from_user.mention} ⦘ 💘 ⋅""") 
+
+
+##############################################################
+##############################################################
+##############################################################
+  
+
+
+#𝙲𝙷.𝚂𝙾𝚄𝚁𝙲𝙴 : @SOURCE_EROR
+#𝙳𝙴𝚅 𝙼𝙰𝚉𝙴𝙽 : @Y_D_ll
+#𝚂𝚄𝙿𝙿𝙾𝚁𝚃 : @SOPER_EROR
+#MOHAMED تم التعديل بواسطة 🎸 ⋅    
