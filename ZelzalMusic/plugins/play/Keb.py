7d7c8a1fd7074626d585ad2d805ceca652a1737c
⏤͟͞ َِ⍣⃟َ𝐓𝐇ِ𝐄 𓆩ََِِ𝐘َِْ𝐎ِِ𝐔ًٌُ𝐒𝐄ُِ𝐅𓆪 <167021222+yousef1yt@users.noreply.github.com>
2024-06-03 07:16:10 +0300

    Create Keb.py

---
+++ ZelzalMusic/plugins/play/Keb.py
@@ -0,0 +1,122 @@
+import asyncio
+from pyrogram import Client, filters
+from strings.filters import command
+from ZelzalMusic.utils.decorators import AdminActual
+from pyrogram.types import (
+    CallbackQuery,
+    InlineKeyboardButton,
+    InlineKeyboardMarkup,
+    ReplyKeyboardMarkup,
+    ReplyKeyboardRemove,
+    InputMediaPhoto,
+    Message,
+)
+from MatrixMusic import app
+
+
+REPLY_MESSAGE = "<b>- اهلا بك عزيزي اليك قائمه الاوامر</b>"
+
+
+
+
+REPLY_MESSAGE_BUTTONS = [
+
+          [
+
+             ("‹ غنيلي ›"),
+
+             ("‹ شعر ›")
+          ],
+
+          [
+
+             ("‹ صور ›"),
+
+             ("‹ انمي ›")
+
+          ],
+
+          [
+
+             ("‹ متحركة ›"),
+
+             ("‹ اقتباسات ›")
+
+          ],
+
+          [
+
+             ("‹ افتارات شباب ›"),
+
+             ("‹ افتار بنات ›")
+
+          ],
+
+          [
+
+             ("‹ هيدرات ›"),
+
+             ("‹ قران ›")
+
+          ],
+    
+          [
+
+            ("‹ جداريات ›"),
+
+            ("‹ لوكيت ›")
+              
+          ],
+          [
+            ("‹ افتارات سينمائية ›"),
+
+            ("‹ افتارات فنانين ›")
+              
+          ],
+          [
+            ("‹ افلام ›"),
+
+            ("‹ قيفات كوكسال ›")
+              
+          ],
+          [
+            ("‹ قيفات شباب ›"),
+
+            ("‹ قيفات بنات ›")
+              
+          ],
+          [
+            ("‹ قيفات قطط ›"),
+
+            ("‹ قيفات اطفال ›")
+              
+          ],
+          [
+            ("‹ قيفات رومانسية ›"),
+
+            ("‹ قيفات كيبوب ›")
+          ],
+          [
+             ("‹ اخفاء الكيبورد ›")
+
+          ]
+
+]
+
+
+
+
+  
+
+@app.on_message(filters.regex("^/cmds$") & filters.private)
+async def cpanel(_, message: Message):             
+        text = REPLY_MESSAGE
+        reply_markup = ReplyKeyboardMarkup(REPLY_MESSAGE_BUTTONS, resize_keyboard=True, selective=True)
+        await message.reply(
+              text=text,
+              reply_markup=reply_markup
+        )
+
+@app.on_message(filters.regex("‹ اخفاء الكيبورد ›") & filters.private)
+async def down(client, message):
+          m = await message.reply("<b>- تم اغلاق الكيبورد.</b>", reply_markup= ReplyKeyboardRemove(selective=True))
