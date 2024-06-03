27173f4adbf77b2684fe39a1ccd6fb8f058515a2
⏤͟͞ َِ⍣⃟َ𝐓𝐇ِ𝐄 𓆩ََِِ𝐘َِْ𝐎ِِ𝐔ًٌُ𝐒𝐄ُِ𝐅𓆪 <167021222+yousef1yt@users.noreply.github.com>
2024-06-03 07:04:56 +0300

    Update start.py

--- ZelzalMusic/utils/inline/start.py
+++ ZelzalMusic/utils/inline/start.py
@@ -1,11 +1,7 @@
-#▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒✯ ʑᴇʟᴢᴀʟ_ᴍᴜsɪᴄ ✯▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒
-#▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒✯  T.me/ZThon   ✯▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒
-#▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒✯ T.me/ZThon_Music ✯▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒
-
 from pyrogram.types import InlineKeyboardButton
 
 import config
-from ZelzalMusic import app
+from MatrixMusic import app
 
 
 def start_panel(_):
@@ -13,11 +9,11 @@ def start_panel(_):
         [
             InlineKeyboardButton(
                 text=_["S_B_1"],
-                url=f"https://t.me/{app.username}?startgroup=true",
+                url=f"https://t.me/{app.username}?startgroup=Commands&admin=ban_users+restrict_members+delete_messages+add_admins+change_info+invite_users+pin_messages+manage_call+manage_chat+manage_video_chats+promote_members",
             )
         ],
         [
-            InlineKeyboardButton(text=_["S_B_6"], url="https://t.me/mmmsc"),
+            InlineKeyboardButton(text=_["S_B_6"], url="https://t.me/KKC8C"),
         ],
     ]
     return buttons
@@ -26,14 +22,34 @@ def start_panel(_):
 def private_panel(_):
     buttons = [
         [
-            InlineKeyboardButton(
+           InlineKeyboardButton(
                 text=_["S_B_3"],
-                url=f"https://t.me/{app.username}?startgroup=true",
-            )
+                url=f"https://t.me/{app.username}?startgroup=Commands&admin=ban_users+restrict_members+delete_messages+add_admins+change_info+invite_users+pin_messages+manage_call+manage_chat+manage_video_chats+promote_members",
+            ),
+           InlineKeyboardButton(
+            text=_["S_B_5"],
+            url=f"https://t.me/y_o_v",
+        )
         ],
-        [InlineKeyboardButton(text=_["S_B_4"], callback_data="zzzback")],
         [
-            InlineKeyboardButton(text=_["S_B_6"], url="https://t.me/mmmsc"),
+           InlineKeyboardButton(
+                text=_["ST_B_3"],
+                callback_data="LG"
+            ),
+           InlineKeyboardButton(
+            text=_["S_B_4"],
+            callback_data="zzzback"
+        )
+        ],
+        [
+             InlineKeyboardButton(
+                text=_["S_B_9"],
+                url=f"https://t.me/KKC8C",
+            ),
+            InlineKeyboardButton(
+                text=_["S_B_6"],
+                url="https://t.me/KKC8C"
+            ),
         ],
     ]
     return buttons
