3b5c10088267c3cb90b5ac4297bace8a2aca9096
⏤͟͞ َِ⍣⃟َ𝐓𝐇ِ𝐄 𓆩ََِِ𝐘َِْ𝐎ِِ𝐔ًٌُ𝐒𝐄ُِ𝐅𓆪 <167021222+yousef1yt@users.noreply.github.com>
2024-06-03 07:14:59 +0300

    Update cmds.py

--- ZelzalMusic/plugins/play/cmds.py
+++ ZelzalMusic/plugins/play/cmds.py
@@ -1,7 +1,3 @@
-#▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒✯ ʑᴇʟᴢᴀʟ_ᴍᴜsɪᴄ ✯▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒
-#▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒✯  T.me/ZThon   ✯▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒
-#▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒✯ T.me/Zelzal_Music ✯▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒
-
 import asyncio
 import os
 import requests
@@ -20,58 +16,26 @@ async def zdatsr(client: Client, message: Message):
     name = usr.first_name
     usrnam = usr.username
     await message.reply_photo(
-        photo=f"https://telegra.ph/file/1bccb452031ac902596bf.jpg",
-        caption=f"""<b>» مرحبـاً بك عـزيـزي </b> {message.from_user.mention} .\n\n<b>» استخـدم الازرار بالاسفـل 𝄞\n» لـ تصفـح اوامـر الميـوزك 🥁</b>""",
+        photo=f"https://graph.org/file/3c9f5ecca9d71df31a0f6.jpg",
+        caption=f"""» مرحبـاً بك عـزيـزي\n» استخـدم الازرار بالاسفـل 𝄞\n» لـ تصفـح اوامـر الميـوزك 🥁</b>""",
         reply_markup=InlineKeyboardMarkup(
             [
                 [
                     InlineKeyboardButton(
-                        "• اوامــر التشغيــل •", callback_data="zzzll"),
-                ],[
+                        "• اوامر التشغيل •", callback_data="zzzll"),
                     InlineKeyboardButton(
-                        "• اوامـر القنـاة •", callback_data="zzzch"),
-                    InlineKeyboardButton(
-                        "• اوامـر الادمـن •", callback_data="zzzad"),
+                        "• اوامر القنوات •", callback_data="zzzch"),
                 ],[
+                    
+                    InlineKeyboardButton(
+                        "• اوامــر الادمــن •", callback_data="zzzad"),
+
                     InlineKeyboardButton(
                         "• اوامــر المطــور •", callback_data="zzzdv"),
-                ],[
-                    InlineKeyboardButton(name, url=f"https://t.me/{usrnam}"),
-                ],[
+                ],[ 
                     InlineKeyboardButton(
-                        "sos Music.", url="https://t.me/mmmsc"),
+                        "• سورس بلاك •", url="https://t.me/KKC8C"),
                 ],
             ]
         ),
     )
-
-
-@app.on_message(command(["مطور", "المطور"]) & filters.group)
-async def zilzal(client: Client, message: Message):
-    usr = await client.get_users(OWNER_ID)
-    name = usr.first_name
-    usrnam = usr.username
-    async for photo in client.iter_profile_photos(OWNER_ID, limit=1):
-                    await message.reply_photo(photo.file_id,       caption=f"""ٴ<b>•────‌‌‏✯ ᴇ_ᴍᴜsɪᴄ ✯──‌‌‏─‌‌‏─•</b>
-                    
-- المطور :[{usr.first_name}](https://t.me/{OWNER})
-- اليوزر :@{usrnam} 
-- الايدي :`{usr.id}`
- 
-ٴ<b>•────‌‌‏✯ ᴇ_ᴍᴜsɪᴄ ✯──‌‌‏─‌‌‏─•</b> """, 
-reply_markup=InlineKeyboardMarkup(
-          [               
-            [            
-              InlineKeyboardButton (name, url=f"https://t.me/{usrnam}"),
-            ],[
-              InlineKeyboardButton("•SoS Music•", url="https://t.me/mmmsc"),
-            ],
-          ] 
-       )                 
-    )                    
-                    sender_id = message.from_user.id
-                    sender_name = message.from_user.first_name
-                    senderuser = message.from_user.username
-                    sender_user = "@{senderuser}" if senderuser else "لا يوجـد"
-                    await app.send_message(OWNER_ID, f"- المستخـدم {message.from_user.mention} يناديـك \n\n- الاسـم : {sender_name} \n- الايـدي : {sender_id}\n- اليـوزر : {sender_user}")
-                    return await app.send_message(LOGGER_ID, f"- المستخـدم {message.from_user.mention} يناديـك \n\n- الاسـم : {sender_name} \n- الايـدي : {sender_id}\n- اليـوزر : {sender_user}")
