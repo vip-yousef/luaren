f4f583b55dee637d564562155a863d0f123e4c9a
⏤͟͞ َِ⍣⃟َ𝐓𝐇ِ𝐄 𓆩ََِِ𝐘َِْ𝐎ِِ𝐔ًٌُ𝐒𝐄ُِ𝐅𓆪 <167021222+yousef1yt@users.noreply.github.com>
2024-06-03 07:34:16 +0300

    Create savm.py

---
+++ ZelzalMusic/plugins/play/savm.py
@@ -0,0 +1,22 @@
+import os
+from pyrogram.types import Message
+from pyrogram import Client, filters
+from MatrixMusic import app
+
+@app.on_message(filters.command("قول", ""))
+async def speak(_, message: Message):
+    chat_id = message.chat.id
+    data = message.text.split(maxsplit=1)
+    if len(data) < 2:return await message.reply("شكولك", reply_to_message_id=message.id)
+    wait = await message.reply('انتظر قليلاً', reply_to_message_id=message.id)
+    if data[1].isascii():
+        language = 'en'
+    else:
+        language = 'ar'
+    audio = gTTS(text=data[1], lang=language)
+    audio.save(f"{message.from_user.username}.mp3")
+	
+    with open(f"{message.from_user.username}.mp3", "rb") as audio:
+        await app.send_voice(chat_id=chat_id, voice=audio, reply_to_message_id=message.id)
+        await wait.delete()
+    os.remove(f"{message.from_user.username}.mp3")
