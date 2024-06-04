0f03318dd3dacebae2be9f8280d5d19745dc5e91
vip-yousef <leynti7@gmail.com>
2024-06-04 09:54:33 +0300

    Update config.py

--- config.py
+++ config.py
@@ -36,7 +36,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/vip-yousef/tatto",
+    "https://github.com/vip-yousef/luaren",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 GIT_TOKEN = getenv(
