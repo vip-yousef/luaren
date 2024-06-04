63c00f96550ef8c5b70f1f5255d14820d5f55201
⏤͟͞ َِ⍣⃟َ𝐓𝐇ِ𝐄 𓆩ََِِ𝐘َِْ𝐎ِِ𝐔ًٌُ𝐒𝐄ُِ𝐅𓆪 <167021222+yousef1yt@users.noreply.github.com>
2024-06-04 07:37:40 +0300

    Update bot.py

--- ZelzalMusic/core/bot.py
+++ ZelzalMusic/core/bot.py
@@ -12,7 +12,7 @@ from ..logging import LOGGER
 
 class Zelzaly(Client):
     def __init__(self):
-        LOGGER("ميــوزك زدثــون").info(f"جارِ بدء تشغيل البوت . . .")
+        LOGGER("ميــوزك بلاك").info(f"جارِ بدء تشغيل البوت . . .")
         super().__init__(
             name="ZelzalMusic",
             api_id=config.API_ID,
@@ -52,7 +52,7 @@ class Zelzaly(Client):
                 "» قم برفـع البـوت مشـرفـاً بكافة الصلاحيات في مجموعـة السجـل"
             )
             exit()
-        LOGGER("ميــوزك زدثــون").info(f" تم بدء تشغيل البوت {self.name} ...✓")
+        LOGGER("ميــوزك بلاك").info(f" تم بدء تشغيل البوت {self.name} ...✓")
 
     async def stop(self):
         await super().stop()
