b9468f81e3efe108003d1ad86404ea36af8dc385
⏤͟͞ َِ⍣⃟َ𝐓𝐇ِ𝐄 𓆩ََِِ𝐘َِْ𝐎ِِ𝐔ًٌُ𝐒𝐄ُِ𝐅𓆪 <167021222+yousef1yt@users.noreply.github.com>
2024-06-04 08:01:15 +0300

    Update Keb2.py

--- ZelzalMusic/plugins/play/Keb2.py
+++ ZelzalMusic/plugins/play/Keb2.py
@@ -9,7 +9,7 @@ from pyrogram import filters
 from pyrogram import Client
 from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton, ReplyKeyboardMarkup
 from strings.filters import command
-from MatrixMusic import (Apple, Resso, SoundCloud, Spotify, Telegram, YouTube, app)
+from ZelzalMusic import (Apple, Resso, SoundCloud, Spotify, Telegram, YouTube, app)
 from MatrixMusic import app
 from random import  choice, randint
 
